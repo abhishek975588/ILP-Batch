611e27a9b9b0858422413b496ea3ce578be936c8
abhishek975588 <bhargavaa013@gmail.com>
2025-03-22 15:42:46 +0530

    initial

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+    {
+        static void Main(string[] args)
+        {
+        int a = 10;
+        Console.WriteLine(a);
+
+        int b = 20;
+        Console.WriteLine(b);
+
+        char v = 'b';
+            Console.WriteLine(v);
+
+            string c = "harry";
+            Console.WriteLine(c);
+
+            double num = 2.3;
+        Console.WriteLine(num);
+
+        bool h = false;
+        Console.WriteLine(h);
+
+
+       
+
+
+            
+        
+
+        }
+    }
+

de9152ee2a7ce6ea69d9ae7a37103617786c23c0
abhishek975588 <bhargavaa013@gmail.com>
2025-03-22 15:45:00 +0530

    Add files via upload

---
+++ ConsoleApp7/ConsoleApp7/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace ConsoleApp7
+{
+    using System;
+
+
+
+    class Student
+    {
+        public String name;
+        public int rollno;
+        public String course;
+        public int age;
+
+
+        public Student(String name, int rollno, String course, int age)
+        {
+
+            this.name = name;
+            this.rollno = rollno;
+            this.course = course;
+            this.age = age;
+
+        }
+
+
+        public void ShowDetail()
+        {
+            Console.WriteLine("name is = " + this.name);
+            Console.WriteLine("Roll-Number is =" + this.rollno);
+            Console.WriteLine("Course =  " + this.course);
+            Console.WriteLine(" Age is  = " + this.age);
+        }
+
+
+    }
+
+
+    class main
+    {
+        public static void Main()
+        {
+
+            Student s1 = new Student("Ankit", 1, "btech", 22);
+            Student s2 = new Student("Aditya", 2, "btech", 22);
+
+            s1.ShowDetail();
+            s2.ShowDetail();
+
+
+        }
+    }
+}
+    
